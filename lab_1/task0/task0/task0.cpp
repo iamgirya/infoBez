3b2c9d6151133719513c405653ee907c6e27ffcf
iamgirya <iamgirya@yandex.ru>
2022-09-08 18:26:41 +0300

    Initial commit

---
+++ lab_1/task0/task0/task0.cpp
@@ -0,0 +1,73 @@
+﻿#include "iostream"
+#include "vector"
+#include <math.h>
+#include <tuple>
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+using namespace std;
+typedef long long ll;
+
+bool isCodsContainsWord(string s, vector<string>& cods) {
+    for (int i = 0; i < cods.size(); i++) {
+        if (s == cods[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isCanDecode(string s, vector<string>& cods) {
+    if (s == "") {
+        return true;
+    }
+    bool flag = false;
+    for (int i = 1; i <= s.length() && i < 7; i++) {
+        if (isCodsContainsWord(s.substr(0, i), cods)) {
+            flag = isCanDecode(s.substr(i), cods);
+            if (flag) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+string encode(string s, vector<string>& cods) {
+    string code = "";
+    for (int i = 0; i < s.length(); i++) {
+        code += cods[s[i] - 65];
+    }
+    return code;
+}
+
+int maxCount = -1;
+void tryToSeparate(string s, vector<string>& cods, int count) {
+    for (int i = 1; i <= s.length() && i < 7; i++) {
+        if (!isCanDecode(s.substr(0, i), cods)) {
+            if (!isCanDecode(s.substr(i), cods)) {
+                if (maxCount < count + 1) {
+                    maxCount = count + 1;
+                }
+            }
+            tryToSeparate(s.substr(i), cods, count + 1);
+        }
+    }
+}
+
+int main()
+{
+    ll k;
+    cin >> k;
+    string s;
+    cin >> s;
+    vector<string> cods = vector<string>(k);
+    for (int i = 0; i < k; i++) {
+        string tmp;
+        cin >> tmp;
+        cods[i] = tmp;
+    }
+
+    tryToSeparate(encode(s, cods), cods, 1);
+    cout << maxCount;
+}
