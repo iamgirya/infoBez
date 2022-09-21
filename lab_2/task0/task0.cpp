df820991a7d1b259ab1922f04374088add2c1cb9
iamgirya <iamgirya@yandex.ru>
2022-09-21 22:55:07 +0300

    Лр2_0

---
+++ lab_2/task0/task0.cpp
@@ -0,0 +1,44 @@
+﻿#include "vector"
+#include <math.h>
+#include <tuple>
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ifstream input("input.txt");
+    string line;
+    getline(input, line);
+    string newLine;
+    int countOfLetter = 1;
+    for (int i = 1; i < line.size(); i++) {
+        if (line[i] == line[i - 1]) {
+            countOfLetter++;
+        }
+        else if (countOfLetter > 1) {
+            newLine += to_string(countOfLetter)+line[i-1];
+            countOfLetter = 1;
+        }
+        else {
+            newLine += line[i - 1];
+            countOfLetter = 1;
+        }
+    }
+    if (countOfLetter > 1) {
+        newLine += to_string(countOfLetter) + line[line.size() - 1];
+        countOfLetter = 1;
+    }
+    else {
+        newLine += line[line.size()-1];
+        countOfLetter = 1;
+    }
+    ofstream output("output.txt");
+    output << newLine;
+}
